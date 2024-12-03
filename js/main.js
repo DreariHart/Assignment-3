1452c013209a4c738550915e1a961b380b3afde2
Taylor Hart <hartt@uleth.ca>
2024-12-03 14:27:08 -0700

    Changed the upgrades into a proper class

--- js/main.js
+++ js/main.js
@@ -3,18 +3,17 @@ var clickMultiply = 1;
 var clickPower = 1;
 var passiveGain = 0.1;
 var cost = 50;
-var upgrades = [1, 2, 3, 4, 5];
-var costs = [50, 100, 200, 400, 800];
 setInterval(passive, 1000);
 var achv1 = false;
 var i = false;
+var upgradeInc = 0;
 
-const clickUpgrade = {
-    cost: 50,
-    power: 1,
-}
+/***************************************
+----------------------------------------
+******** Ball Falling Animation ********
+----------------------------------------
+****************************************/
 
-/**********Ball Falling Animation **********/
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -23,14 +22,17 @@ let raf;
 let balls = [];
 class ball {
     constructor() {
-        this.x = 115,
+        this.x = 50,
             this.y = 9,
             this.vx = 2,
             this.vy = 3,
             this.radius = 15;
+            this.firstBounce = false;
     }
     draw() {
+
         ctx.drawImage(image, this.x, this.y, 15, 15);
+
     }
     update() {
         this.x += this.vx;
@@ -38,10 +40,17 @@ class ball {
         this.vy *= 0.99;
         this.vy += 0.25;
         if (
-            this.y + this.vy > canvas.height - this.radius ||
+            this.y + this.vy > (canvas.height + 5) - this.radius ||
             this.y + this.vy < this.radius
+
         ) {
-            this.vy = -(this.vy / 1.5);
+            if (this.firstBounce == false) {
+            this.vy = -(this.vy / Math.max(Math.random()*2, 0.9));
+            this.firstBounce = true;
+            }
+            else {
+                this.vy = -(this.vy / 1.2);
+            }
         }
     }
 };
@@ -75,7 +84,11 @@ function animateBall() {
     balls.push(new ball());
 }
 
-/**********Passive Gain Functions **********/
+/***************************************
+----------------------------------------
+*********** Passive Functions **********
+----------------------------------------
+****************************************/
 
 function passive() {
     count = count + passiveGain;
@@ -85,14 +98,6 @@ function passive() {
     checkAchievment();
 }
 
-function checkUpgrade() {
-    if (count >= cost) {
-        $("#upgrade1").css("background", "green");
-    } else {
-        $("#upgrade1").css("background", "red");
-    }
-}
-
 function checkAchievment() {
     if (count == 1 && achv1 == false) {
         $("#achv1").css("background", "green");
@@ -100,13 +105,19 @@ function checkAchievment() {
     }
 }
 
+/***************************************
+----------------------------------------
+************* Click Functions **********
+----------------------------------------
+****************************************/
+
 function clicked() {
     count = count + (clickPower * clickMultiply);
     $("#count").text(Math.trunc(count));
     checkUpgrade();
     clickNumber();
-    if(i == false) {animateBallFirst();}
-    else {animateBall();}
+    if (i == false) { animateBallFirst(); }
+    else { animateBall(); }
 }
 
 function clickNumber() {
@@ -114,17 +125,74 @@ function clickNumber() {
     //$("#cNum").text(clickGain); 
 }
 
-$("#upgrade1").on("click", upgrade);
-function upgrade() {
-    if (count >= cost) {
-        count = count - cost;
-        clickPower = clickPower + 1;
-        cost = cost + 30;
-        $("#count").text(Math.trunc(count));
-        $("#cost1").text(cost);
-        checkUpgrade();
+/***************************************
+----------------------------------------
+******* Upgrade Functionality **********
+----------------------------------------
+****************************************/
+
+let upgrades = [];
+
+class clickUpgrade {
+    constructor() {
+        this.id = "cUp";
+        this.cost = 50,
+            this.increase = 30,
+            this.canBuy = false;
+            this.name = "Upgrade 1";
+        this.background = "red";
+    }
+    createUpgrade() {
+        $("#clickUp").append("<div class='upgradeItem' id=" + this.id + "> <h3>" + this.name + "</h3> <p id='cost"+ this.id + "'>Cost: " + this.cost + "</p></div>");
+        $("#" + this.id).css("background", this.background);
+    }
+    upgrade() {
+        if (count >= this.cost) {
+            count = count - this.cost;
+            clickPower = clickPower + 1;
+            this.cost = this.cost + 30;
+            $("#count").text(Math.trunc(count));
+            $("#cost" + this.id).text("Cost: " + this.cost);
+            checkUpgrade();
+        }
+    }
+    
+    checkBuy() {
+        if (count >= this.cost) {
+            $("#" + this.id).css("background", "green");
+        } else {
+            $("#" + this.id).css("background", "red");
+        }
+    }
+};
+
+function checkUpgrade() {
+    if (upgradeInc == 0 && count >= 1) {
+        upgrades.push(new clickUpgrade("cUp1", 50, 30, false, "The First One", "yellow"));
+        upgrades[0].createUpgrade();
+        upgradeInc = 1;
+    } else if (upgradeInc == 1 && count >= 100) {
+        upgrades.push(new clickUpgrade("cUp2", 50, 30, false, "The Second One", "yellow"));
+        upgrades[1].createUpgrade();
+        upgradeInc = 2;
+    }
+
+    for (let clickUpgrade of upgrades) {
+        clickUpgrade.checkBuy();
     }
 }
 
-$("#c").on("click", clicked);
 
+
+
+
+/*********** Main JQuery Procedure **********/
+
+$("#c").on("click", clicked);
+$("#clickUp").on("click", function() {
+    if (upgrades.length > 0) {
+        $("#" + upgrades[0].id).on("click", upgrades[0].upgrade());
+    } else if (upgrades.length > 1) {
+        $("#" + upgrades[1].id).on("click", upgrades[1].upgrade());
+    }
+});
